5993393c189a596021e40363313d6205c2eeb4cf
Antragon <68800091+Antragon@users.noreply.github.com>
2021-12-22 09:39:41 +0100

    Moved TextLocalizationEditor to collections

---
+++ Localization/LocalizableComponents/Editor/TextLocalizationEditor.cs
@@ -0,0 +1,26 @@
+﻿namespace Collections.Localization.LocalizableComponents.Editor {
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [CustomEditor(typeof(TextLocalization))]
+    public class TextLocalizationEditor : Editor {
+        private Text _text;
+        private TextLocalization _textLocalization;
+
+        private void OnEnable() {
+            _textLocalization = (TextLocalization)target;
+            _text = _textLocalization.GetComponentInChildren<Text>();
+        }
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            if (Application.isPlaying) return;
+            if (_textLocalization.LocalizableValue is LocalizableString localizableString) {
+                _text.text = $"{localizableString.Path}.{localizableString.ValueKey}";
+                PrefabUtility.RecordPrefabInstancePropertyModifications(_text);
+            }
+        }
+    }
+}
