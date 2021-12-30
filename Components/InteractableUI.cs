bc94381706a45366cc9ecf473b273d617878f1b6
Antragon <68800091+Antragon@users.noreply.github.com>
2021-12-30 20:14:56 +0100

    Added InteractableUI

---
+++ Components/InteractableUI.cs
@@ -0,0 +1,14 @@
+﻿namespace Collections.Components {
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// Mouse Events need any type of raycast target. A transparent image will do, but this should create less overhead
+    /// </summary>
+    public class InteractableUI : Graphic {
+        protected override void Awake() {
+            base.Awake();
+            color = Color.clear;
+        }
+    }
+}
