c9f41c93e1f2d49242a4168d5bc91c2bfd57f3eb
Antragon <unity@eilix.de>
2022-06-19 10:19:24 +0200

    Extended inspector for localizable components

---
+++ Editor/Localization/DropdownLocalizationEditor.cs
@@ -0,0 +1,32 @@
+﻿namespace Collections.Editor.Localization {
+    using System.Linq;
+    using Collections.Localization;
+    using Collections.Localization.LocalizableComponents;
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [CustomEditor(typeof(DropdownLocalization))]
+    public class DropdownLocalizationEditor : Editor {
+        private Dropdown _dropdown;
+        private DropdownLocalization _dropdownLocalization;
+
+        private void OnEnable() {
+            _dropdownLocalization = (DropdownLocalization)target;
+            _dropdown = _dropdownLocalization.GetComponent<Dropdown>();
+        }
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            if (Application.isPlaying) return;
+            if (_dropdownLocalization.LocalizableValues is null) return;
+
+            var optionDataList = _dropdownLocalization.LocalizableValues
+                .Cast<LocalizableString>()
+                .Select(x => new Dropdown.OptionData($"{x.Path}.{x.ValueKey}"))
+                .ToList();
+            _dropdown.options = optionDataList;
+        }
+    }
+}
---
+++ Editor/PropertyDrawers/LocalizableStringDrawer.cs
@@ -0,0 +1,60 @@
+﻿namespace Collections.Editor.PropertyDrawers {
+    using Collections.Localization;
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(LocalizableString))]
+    public class LocalizableStringDrawer : PropertyDrawer {
+        private const string _pathName = nameof(LocalizableString.Path);
+        private const string _valueKeyName = nameof(LocalizableString.ValueKey);
+
+        private readonly string _propertyLabel = ObjectNames.NicifyVariableName(nameof(LocalizableString));
+
+        private Rect _rect;
+        private SerializedProperty _property;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            _rect = EditorGUI.PrefixLabel(position, new GUIContent(_propertyLabel));
+            _property = property;
+            var indentLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            DrawProperty();
+            EditorGUI.indentLevel = indentLevel;
+        }
+
+        private void DrawProperty() {
+            DrawPath();
+            DrawSeparator();
+            DrawValueKey();
+        }
+
+        private void DrawPath() {
+            var rect = _rect;
+            var property = GetProperty(_pathName);
+            var guiStyle = new GUIStyle(GUI.skin.textField);
+            guiStyle.CalcMinMaxWidth(new GUIContent(property.stringValue), out var min, out _);
+            rect.width = Mathf.Max(min, 16);
+            property.stringValue = EditorGUI.TextField(rect, property.stringValue);
+            _rect.x += rect.width;
+            _rect.width -= rect.width;
+        }
+
+        private void DrawSeparator() {
+            var rect = _rect;
+            rect.width = 6;
+            EditorGUI.LabelField(rect, ".");
+            _rect.x += rect.width;
+            _rect.width -= rect.width;
+        }
+
+        private void DrawValueKey() {
+            var rect = _rect;
+            var property = GetProperty(_valueKeyName);
+            property.stringValue = EditorGUI.TextField(rect, property.stringValue);
+        }
+
+        private SerializedProperty GetProperty(string childPropertyName) {
+            return _property.FindPropertyRelative($"<{childPropertyName}>k__BackingField");
+        }
+    }
+}
