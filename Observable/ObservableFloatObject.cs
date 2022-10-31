f18652bbdb637890e75f88a415c23fced9b8562d
Antragon <unity@eilix.de>
2022-10-31 14:45:23 +0100

    Add observable Objects

---
+++ Observable/ObservableFloatObject.cs
@@ -0,0 +1,6 @@
+﻿namespace Collections.Observable {
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = nameof(ObservableFloatObject), menuName = nameof(ScriptableObject) + "/" + nameof(ObservableFloatObject), order = 0)]
+    public class ObservableFloatObject : ObservableObject<float> { }
+}
---
+++ Observable/ObservableObject.cs
@@ -0,0 +1,58 @@
+﻿namespace Collections.Observable {
+    using System;
+    using UnityEngine;
+
+    public class ObservableObject<T> : ScriptableObject {
+        private readonly ObservableAction<T> _onValueChanged = new();
+
+        private T _value;
+
+        public T Value {
+            get => _value;
+            set => SetValue(value);
+        }
+
+        public void SetValue(T value) {
+            if (Equals(_value, value)) return;
+            SetAndInvoke(value);
+        }
+
+        public void SetAndInvoke(T value) {
+            _value = value;
+            Invoke();
+        }
+
+        public void Invoke() {
+            _onValueChanged.Invoke(_value);
+        }
+
+        public void SetSilently(T value) {
+            _value = value;
+        }
+
+        public ObservableObject<T> AddListener(Action<T> onValueChanged) {
+            _onValueChanged.AddListener(onValueChanged);
+            return this;
+        }
+
+        public ObservableObject<T> AddAndInvokeListener(Action<T> onValueChanged) {
+            _onValueChanged.AddListener(onValueChanged);
+            onValueChanged(Value);
+            return this;
+        }
+
+        public ObservableObject<T> ListenOnce(Action<T> onValueChanged) {
+            _onValueChanged.ListenOnce(onValueChanged);
+            return this;
+        }
+
+        public ObservableObject<T> RemoveListener(Action<T> onValueChanged) {
+            _onValueChanged.RemoveListener(onValueChanged);
+            return this;
+        }
+
+        public override string ToString() {
+            return $"<{nameof(ObservableProperty<T>)}>{Value}";
+        }
+    }
+}
