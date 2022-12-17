1b534cf2ab6094b7548c069d7b303852e1af42f8
Antragon <unity@eilix.de>
2022-12-17 12:54:37 +0100

    InteractableUI requires CanvasRenderer

--- Components/InteractableUI.cs
+++ Components/InteractableUI.cs
@@ -5,6 +5,7 @@
     /// <summary>
     /// Mouse Events need any type of raycast target. A transparent image will do, but this should create less overhead
     /// </summary>
+    [RequireComponent(typeof(CanvasRenderer))]
     public class InteractableUI : Graphic {
         protected override void Awake() {
             base.Awake();
