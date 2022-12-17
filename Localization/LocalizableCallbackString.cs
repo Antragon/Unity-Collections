04edb7de81ef55c4c316f53f890d35fe224f4109
Antragon <unity@eilix.de>
2022-12-17 14:51:34 +0100

    Add localizable callback values

---
+++ Localization/LocalizableCallbackString.cs
@@ -0,0 +1,25 @@
+﻿namespace Collections.Localization {
+    using System;
+    using UnityEngine;
+
+    public class LocalizableCallbackString : ILocalizableValue {
+        private readonly string _path;
+        private readonly Func<string> _getValueKey;
+
+        public LocalizableCallbackString(string path, Func<string> getValueKey) {
+            _path = path;
+            _getValueKey = getValueKey;
+        }
+
+        public string GetLocalizedValue() {
+            if (!LocalizationRepository.Self.TryGetLocalizedValue(new ValueLocalization(_path, _getValueKey()), out var localizedValue, out var message)) {
+                Debug.LogWarning(message);
+                return DefaultValue();
+            }
+
+            return localizedValue;
+        }
+
+        private string DefaultValue() => $"{_path}.{_getValueKey()}";
+    }
+}
---
+++ Localization/UnlocalizedCallbackValue.cs
@@ -0,0 +1,13 @@
+﻿namespace Collections.Localization {
+    using System;
+
+    public class UnlocalizedCallbackValue : ILocalizableValue {
+        private readonly Func<string> _getValue;
+
+        public UnlocalizedCallbackValue(Func<string> getValue) {
+            _getValue = getValue;
+        }
+
+        public string GetLocalizedValue() => _getValue();
+    }
+}
