7ea92e004fb21ef467a80ec0f11f3ebf3777daf5
Antragon <unity@eilix.de>
2023-01-16 08:07:08 +0100

    Add extensions method

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Collections.Extensions {
+    using System;
+    using System.Linq;
+
+    public static class EnumExtensions {
+        public static T[] GetValues<T>() {
+            return Enum.GetValues(typeof(T)).Cast<T>().ToArray();
+        }
+    }
+}
