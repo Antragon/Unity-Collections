d3d58c298b20f767fa14ead160fb6508b801b522
Antragon <unity@eilix.de>
2023-10-27 06:34:35 +0200

    Add Extension method

---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Collections.Extensions {
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class EnumerableExtensions {
+        public static bool In<T>(this T value, IEnumerable<T> enumerable) {
+            return enumerable.Contains(value);
+        }
+    }
+}
