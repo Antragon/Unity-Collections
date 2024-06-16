b7d15d5972a13b795b1d0a693c060e96d4744012
Antragon <unity@eilix.de>
2024-06-16 12:29:41 +0200

    Add extension method

--- Extensions/EnumerableExtensions.cs
+++ Extensions/EnumerableExtensions.cs
@@ -6,9 +6,14 @@
         public static bool In<T>(this T value, params T[] enumerable) {
             return enumerable.Contains(value);
         }
-        
+
         public static bool In<T>(this T value, IEnumerable<T> enumerable) {
             return enumerable.Contains(value);
+        }
+
+        public static T Random<T>(this IList<T> enumerable) {
+            var index = UnityEngine.Random.Range(0, enumerable.Count);
+            return enumerable[index];
         }
     }
 }
