087cb20cc056c2e885f0c5d81b8a9e9ce211dddd
Antragon <unity@eilix.de>
2025-04-03 09:09:59 +0200

    Add Equatable to ComponentCache

--- Components/ComponentCache.cs
+++ Components/ComponentCache.cs
@@ -4,7 +4,7 @@
     using System.Diagnostics.CodeAnalysis;
     using UnityEngine;
 
-    public class ComponentCache {
+    public class ComponentCache : IEquatable<ComponentCache> {
         private readonly Dictionary<Type, object?> _cache = new();
 
         public ComponentCache(GameObject gameObject) {
@@ -32,6 +32,23 @@
             var componentCache = new ComponentCache(baseComponent.gameObject);
             componentCache._cache.Add(typeof(T), baseComponent);
             return componentCache;
+        }
+
+        public bool Equals(ComponentCache? other) {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return GameObject.Equals(other.GameObject);
+        }
+
+        public override bool Equals(object? obj) {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((ComponentCache)obj);
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(GameObject);
         }
     }
 }
