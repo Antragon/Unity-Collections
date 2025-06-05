5d3610d113df8b93be8ca1d1bd4d820fd3c6cbb7
Antragon <unity@eilix.de>
2025-06-05 07:39:20 +0200

    Add extensions method

--- Observable/ObservableExtensions.cs
+++ Observable/ObservableExtensions.cs
@@ -7,5 +7,21 @@
             disposables.ForEach(d => d.Dispose());
             disposables.Clear();
         }
+
+        public static ObservableCallback<T> AddAndInvokeListener<T>(this IObservable<T> observable, Action action) {
+            return observable.AddAndInvokeListener(_ => action(), default!);
+        }
+
+        public static ObservableCallback<T> AddListener<T>(this IObservable<T> observable, Action action) {
+            return observable.AddListener(_ => action());
+        }
+
+        public static ObservableValueCallback<T> AddAndInvokeListener<T>(this IObservableValue<T> observableValue, Action action) {
+            return observableValue.AddAndInvokeListener(_ => action());
+        }
+
+        public static ObservableValueCallback<T> AddListener<T>(this IObservableValue<T> observable, Action action) {
+            return observable.AddListener(_ => action());
+        }
     }
 }
