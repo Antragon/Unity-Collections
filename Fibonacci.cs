75b1a28bb515438c3af8c6fe3465ae439f2e7747
Antragon <unity@eilix.de>
2026-06-23 06:50:31 +0200

    Add Fibonacci sequence, just for fun

---
+++ Fibonacci.cs
@@ -0,0 +1,15 @@
+﻿namespace Collections {
+    using System.Collections.Generic;
+
+    public static class Fibonacci {
+        private static readonly List<int> _fibonacciList = new() { 1, 2, 3 };
+
+        public static int GetNumber(int index) {
+            while (_fibonacciList.Count <= index) {
+                _fibonacciList.Add(_fibonacciList[index - 1] + _fibonacciList[index - 2]);
+            }
+
+            return _fibonacciList[index];
+        }
+    }
+}
